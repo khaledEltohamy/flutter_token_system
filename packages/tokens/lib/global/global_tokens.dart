d44c6a185c8fd6e76286d027539559c4bb5e21f5
khaledEltohamy <khaledEltohamy@users.noreply.github.com>
2026-04-03 11:09:19 +0000

    chore: update design tokens (global + alias)

--- packages/tokens/lib/global/global_tokens.dart
+++ packages/tokens/lib/global/global_tokens.dart
@@ -3,4 +3,34 @@ import 'dart:ui';
 
 class GlobalTokens {
   GlobalTokens._();
+  static const Color primary_50 = Color(0xFFEEF4FF);
+  static const Color primary_100 = Color(0xFFD9E4FF);
+  static const Color primary_200 = Color(0xFFB3C9FF);
+  static const Color primary_300 = Color(0xFF8DAEFF);
+  static const Color primary_400 = Color(0xFF6693FF);
+  static const Color primary_500 = Color(0xFF3366FF);
+  static const Color primary_600 = Color(0xFF254EDB);
+  static const Color primary_700 = Color(0xFF1939B7);
+  static const Color primary_800 = Color(0xFF102993);
+  static const Color primary_900 = Color(0xFF091B7A);
+  static const Color gray_0 = Color(0xFFFFFFFF);
+  static const Color gray_50 = Color(0xFFF9FAFB);
+  static const Color gray_100 = Color(0xFFF3F4F6);
+  static const Color gray_200 = Color(0xFFE5E7EB);
+  static const Color gray_300 = Color(0xFFD1D5DB);
+  static const Color gray_400 = Color(0xFF9CA3AF);
+  static const Color gray_500 = Color(0xFF6B7280);
+  static const Color gray_600 = Color(0xFF4B5563);
+  static const Color gray_700 = Color(0xFF374151);
+  static const Color gray_800 = Color(0xFF1F2937);
+  static const Color gray_900 = Color(0xFF111827);
+  static const Color red_100 = Color(0xFFFEE2E2);
+  static const Color red_500 = Color(0xFFEF4444);
+  static const Color red_700 = Color(0xFFB91C1C);
+  static const Color green_100 = Color(0xFFD1FAE5);
+  static const Color green_500 = Color(0xFF10B981);
+  static const Color green_700 = Color(0xFF047857);
+  static const Color yellow_100 = Color(0xFFFEF3C7);
+  static const Color yellow_500 = Color(0xFFF59E0B);
+  static const Color yellow_700 = Color(0xFFB45309);
 }
