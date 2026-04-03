e48f9900354c2ba9aba9525bf2357cc239029cc3
khaledEltohamy <khaledEltohamy@users.noreply.github.com>
2026-04-03 10:56:00 +0000

    chore: update design tokens (global + alias)

--- packages/tokens/lib/global/global_tokens.dart
+++ packages/tokens/lib/global/global_tokens.dart
@@ -3,12 +3,4 @@ import 'dart:ui';
 
 class GlobalTokens {
   GlobalTokens._();
-  static const Color primary = Color(0xFF131F8C);
-  static const Color secoundry_primary = Color(0xFF332323);
-  static const Color background = Color(0xFFF4F4F4);
-  static const Color sechme_color = Color(0xFF828317);
-  static const Color border_color = Color(0xFFD1CCCC);
-  static const Color test_color = Color(0xFF574141);
-  static const Color new_test_color = Color(0xFFE98484);
-  static const Color brown_color = Color(0xFF3E2323);
 }
