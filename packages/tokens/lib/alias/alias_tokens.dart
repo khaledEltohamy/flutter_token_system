93a27f4a305792724dfd816bda327dfaab80dc8a
khaledEltohamy <khaledEltohamy@users.noreply.github.com>
2026-04-04 08:49:12 +0000

    chore: update design tokens (global + alias)

---
+++ packages/tokens/lib/alias/alias_tokens.dart
@@ -0,0 +1,41 @@
+// GENERATED FILE - DO NOT EDIT
+import 'dart:ui';
+import '../global/global_tokens.dart';
+
+class AliasTokens {
+  AliasTokens._();
+  static const Color background_primary = GlobalTokens.gray_0;
+  static const Color background_secondary = GlobalTokens.gray_50;
+  static const Color background_tertiary = GlobalTokens.gray_100;
+  static const Color background_inverse = GlobalTokens.gray_900;
+  static const Color surface_primary = GlobalTokens.gray_0;
+  static const Color surface_secondary = GlobalTokens.gray_50;
+  static const Color text_primary = GlobalTokens.gray_900;
+  static const Color text_secondary = GlobalTokens.gray_600;
+  static const Color text_tertiary = GlobalTokens.gray_500;
+  static const Color text_disabled = GlobalTokens.gray_400;
+  static const Color text_inverse = GlobalTokens.gray_0;
+  static const Color icon_primary = GlobalTokens.gray_900;
+  static const Color icon_secondary = GlobalTokens.gray_500;
+  static const Color icon_disabled = GlobalTokens.gray_400;
+  static const Color icon_inverse = GlobalTokens.gray_0;
+  static const Color border_primary = GlobalTokens.gray_200;
+  static const Color border_secondary = GlobalTokens.gray_100;
+  static const Color border_focus = GlobalTokens.primary_500;
+  static const Color divider_default = GlobalTokens.gray_200;
+  static const Color button_primary_background = GlobalTokens.primary_500;
+  static const Color button_primary_hover = GlobalTokens.primary_600;
+  static const Color button_primary_pressed = GlobalTokens.primary_700;
+  static const Color button_primary_text = GlobalTokens.gray_0;
+  static const Color button_primary_disabled = GlobalTokens.gray_300;
+  static const Color button_secondary_background = GlobalTokens.gray_100;
+  static const Color button_secondary_text = GlobalTokens.gray_900;
+  static const Color status_success_background = GlobalTokens.green_100;
+  static const Color status_success_content = GlobalTokens.green_700;
+  static const Color status_error_background = GlobalTokens.red_100;
+  static const Color status_error_content = GlobalTokens.red_700;
+  static const Color status_warning_background = GlobalTokens.yellow_100;
+  static const Color status_warning_content = GlobalTokens.yellow_700;
+  static const Color status_info_background = GlobalTokens.blue_100;
+  static const Color status_info_content = GlobalTokens.blue_700;
+}
